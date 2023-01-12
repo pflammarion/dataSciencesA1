5cd45c1a6bd213b563fd5e706cebb1a8dcdc0a83
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-12 11:40:19 +0100

    add stand matrix

--- TD1/exercise.py
+++ TD1/exercise.py
@@ -1,51 +1,18 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-import numpy as np
+
+
+def standardisation(inputVal):
+    average = inputVal.mean()
+    standardDeviation = inputVal.std()
+    return (inputVal - average) / standardDeviation
 
 
 # Load the dataset
-data = pd.read_table("exercice.csv", delimiter=";")
+df = pd.read_table("exercice.csv", delimiter=";")
 
-# Select the columns you want to standardize
-columns = ['X1', 'X2']
+stand = df
+stand['X1'] = standardisation(df['X1'])
 
-# Standardize the selected columns
-scaler = StandardScaler()
-scaler.fit(data[columns])
-data[columns] = scaler.transform(data[columns])
+stand['X2'] = standardisation(df['X2'])
 
-# Calculate the variance matrix
-variance_matrix = data[columns].cov()
-
-#Diagonalize the variance matrix
-eigenvalues, eigenvectors = np.linalg.eig(variance_matrix)
-
-# Find the principal component loading vectors
-loading_vectors = eigenvectors * eigenvalues
-
-print('\nStandardized\n')
-print(data)
-print('\nVariance\n')
-print(variance_matrix)
-
-print('\neigenvalues\n')
-print(eigenvalues)
-
-print('\neigenvectors\n')
-print(eigenvectors)
-
-print('\nloading_vectors\n')
-print(loading_vectors)
-
-
-data = pd.read_table("exercice.csv", delimiter=";")
-
-
-##for i, col in enumerate(columns):
-    #data[col] = data[col].apply(lambda x: x * loading_vectors[i])
-
-#print(data['X1'])
-
-
-
-
+print(df)
