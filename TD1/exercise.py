eb6476fc401fdbafb5eaedb00934e8501b5f0cfb
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-12 11:40:19 +0100

    add stand matrix

--- TD1/exercise.py
+++ TD1/exercise.py
@@ -1,4 +1,18 @@
 import pandas as pd
 
+
+def standardisation(inputVal):
+    average = inputVal.mean()
+    standardDeviation = inputVal.std()
+    return (inputVal - average) / standardDeviation
+
+
 # Load the dataset
-data = pd.read_csv('my_data.csv')
+df = pd.read_table("exercice.csv", delimiter=";")
+
+stand = df
+stand['X1'] = standardisation(df['X1'])
+
+stand['X2'] = standardisation(df['X2'])
+
+print(df)
