bf2bf37df1b62085a04bb7cee7098fa9732b4e93
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-19 11:27:01 +0100

    ex2

--- TD2/main.py
+++ TD2/main.py
@@ -1,5 +1,29 @@
 import pandas as pd
-
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
 
 # Load the dataset
-data = pd.read_table("ozone.csv", delimiter=" ")
+oz = pd.read_csv("ozone.csv", delimiter=" ")
+
+#ex1
+
+print("Number of observations: ", oz.shape[0])
+print("\nNumber of variables: ", oz.shape[1])
+
+#ex2
+
+print("\nDescriptive statistics\n", oz.describe())
+
+scatter_matrix(oz, alpha=0.2, figsize=(6, 6), diagonal='hist')
+plt.show()
+
+corr_matrix = oz.corr()
+
+print('\nCorr matrix\n', corr_matrix)
+
+correlation = corr_matrix['maxO3']
+
+correlation = correlation.sort_values(ascending=False)
+
+print('\nVariable which is correlated the most with the ozone content maxO3\n\n', correlation)
+
