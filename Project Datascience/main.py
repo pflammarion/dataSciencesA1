a9ba6a8b514c4f441b97021bd142df794b28014a
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-19 15:35:40 +0100

    clean main

--- Project Datascience/main.py
+++ Project Datascience/main.py
@@ -7,7 +7,7 @@ import scipy.stats as stats
 
 #uploading dataset to python
 
-data_live = pd.read_csv("Live_20210128.csv", delimiter=",")
+data_live = pd.read_csv("Live_20210128_clean.csv", delimiter=",")
 
 #observations + variables
 
@@ -15,18 +15,6 @@ print(data_live)
 
 print("Number of observations: ", data_live.shape[0])
 print("\nNumber of variables: ", data_live.shape[1])
-
-#removing the unecessary variables (does include id since it's the same as the index)
-
-data_live.pop('status_id')
-data_live.pop('Column1')
-data_live.pop('Column2')
-data_live.pop('Column3')
-data_live.pop('Column4')
-
-print("\nRemoved status_id, Column1, Column2, Column3, Column4")
-
-print(data_live)
 
 #calculating descriptive statistics for all the variables
 
@@ -39,6 +27,8 @@ print('\n Descriptive stats of the variable num_wows:\n',data_live['num_wows'].d
 print('\n Descriptive stats of the variable num_hahas:\n',data_live['num_hahas'].describe())
 print('\n Descriptive stats of the variable num_sads:\n',data_live['num_sads'].describe())
 print('\n Descriptive stats of the variable num_angrys:\n',data_live['num_angrys'].describe())
+
+
 
 
 
