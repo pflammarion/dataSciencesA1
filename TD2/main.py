5cc600a464a0997fa0ccc3036bce0a6e5d38ac1f
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-19 12:11:45 +0100

    save

--- TD2/main.py
+++ TD2/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
+import statsmodels.formula.api as smf
 
 # Load the dataset
 oz = pd.read_csv("ozone.csv", delimiter=" ")
@@ -27,6 +28,18 @@ correlation = corr_matrix['maxO3']
 correlation = correlation.sort_values(ascending=False)
 
 print('\nVariable which is correlated the most with the ozone content maxO3\n\n', correlation)
+
+#ex4
+
+oz_regsimple = smf.ols(formula='maxO3 ~ Ne12', data=oz).fit()
+print('\nConfidence interval for the parameter β1\n', oz_regsimple.conf_int(alpha=0.1))
+
+#ex5
+
+oz_regmult = smf.ols(formula='maxO3 ~ Ne12', data=oz).fit()
+print('\nSummary report of the fitting\n', oz_regmult.summary())
+## n'est pas la bonne commande => reg simple
+
 
 
 
