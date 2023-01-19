934badf9adeb8896dc1a0fe6205837f50ece447a
Sh0lf <vyvincentyap@gmail.com>
2023-01-19 12:11:54 +0100

    fix char functions.py

--- TD2/main.py
+++ TD2/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
+import numpy as np
 from scipy.stats import gaussian_kde
 
 # Load the dataset
@@ -28,5 +29,23 @@ correlation = correlation.sort_values(ascending=False)
 
 print('\nVariable which is correlated the most with the ozone content maxO3\n\n', correlation)
 
+#ex3
 
+import statsmodels.api as sm
+lm = sm.OLS.from_formula('maxO3 ~ Ne12', oz)
+oz_regsimple = lm. fit ()
+print (oz_regsimple.summary())
 
+plt.scatter(oz.Ne12, oz.maxO3)
+
+slope = np.polyfit(oz.Ne12, oz.maxO3, 1)[0]
+intercept = np.polyfit(oz.Ne12, oz.maxO3, 1)[1]
+
+#plt.plot([x,x], [x,x], 'k−', color = 'r') #plot 2 points of the fitted line
+def abline(slope, intercept):
+    axes = plt.gca()
+    x_vals = np.array(axes.get_xlim())
+    y_vals = intercept + slope * x_vals
+    plt.plot(x_vals, y_vals, '--')
+
+abline(slope, intercept)
