59927bbebd2ae63d655f6bf6f2832cbc453be3cc
Paul Flammarion <pa.flammarion@gmail.com>
2023-01-19 11:39:45 +0100

    courbe a la place d hist

--- TD2/main.py
+++ TD2/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
+from scipy.stats import gaussian_kde
 
 # Load the dataset
 oz = pd.read_csv("ozone.csv", delimiter=" ")
@@ -14,7 +15,7 @@ print("\nNumber of variables: ", oz.shape[1])
 
 print("\nDescriptive statistics\n", oz.describe())
 
-scatter_matrix(oz, alpha=0.2, figsize=(6, 6), diagonal='hist')
+scatter_matrix(oz, alpha=0.2, figsize=(6, 6), diagonal='kde', color="red")
 plt.show()
 
 corr_matrix = oz.corr()
@@ -26,4 +27,6 @@ correlation = corr_matrix['maxO3']
 correlation = correlation.sort_values(ascending=False)
 
 print('\nVariable which is correlated the most with the ozone content maxO3\n\n', correlation)
+
+
 
