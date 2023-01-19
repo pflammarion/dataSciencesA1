d146d1dd9de0bb555147a38443bd9970a8db299f
Sh0lf <vyvincentyap@gmail.com>
2023-01-19 16:41:36 +0100

    MORE PLOOOOOTS

--- Project Datascience/main.py
+++ Project Datascience/main.py
@@ -47,3 +47,77 @@ plt.show()
 
 df[columns2].boxplot()
 plt.show()
+
+counts_num_reactions = df["num_reactions"].value_counts()
+counts_num_comments = df["num_comments"].value_counts()
+counts_num_shares = df["num_shares"].value_counts()
+counts_num_likes = df["num_likes"].value_counts()
+counts_num_loves = df["num_loves"].value_counts()
+counts_num_wows = df["num_wows"].value_counts()
+counts_num_hahas = df["num_hahas"].value_counts()
+counts_num_sads = df["num_sads"].value_counts()
+counts_num_angrys = df["num_angrys"].value_counts()
+
+counts_num_r = counts_num_reactions.index
+counts_num_c = counts_num_comments.index
+counts_num_s = counts_num_shares.index
+counts_num_li = counts_num_likes.index
+counts_num_lo = counts_num_loves.index
+counts_num_w = counts_num_wows.index
+counts_num_h = counts_num_hahas.index
+counts_num_sa = counts_num_sads.index
+counts_num_a = counts_num_angrys.index
+
+values_num_r = counts_num_reactions.values
+values_num_c = counts_num_comments.values
+values_num_s = counts_num_shares.values
+values_num_li = counts_num_likes.values
+values_num_lo = counts_num_loves.values
+values_num_w = counts_num_wows.values
+values_num_h = counts_num_hahas.values
+values_num_sa = counts_num_sads.values
+values_num_a = counts_num_angrys.values
+
+
+plt.scatter(counts_num_r, values_num_r, c='blue', label="reactions")
+plt.show()
+plt.scatter(counts_num_c, values_num_c, c='blue', label="comments")
+plt.show()
+plt.scatter(counts_num_s, values_num_s, c='blue', label="share")
+plt.show()
+plt.scatter(counts_num_li, values_num_li, c='blue', label="likes")
+plt.show()
+plt.scatter(counts_num_lo, values_num_lo, c='blue', label="loves")
+plt.show()
+plt.scatter(counts_num_w, values_num_w, c='blue', label="wows")
+plt.show()
+plt.scatter(counts_num_h, values_num_h, c='blue', label="hahas")
+plt.show()
+plt.scatter(counts_num_sa, values_num_sa, c='blue', label="sads")
+plt.show()
+plt.scatter(counts_num_a, values_num_a, c='blue', label="angrys")
+plt.show()
+
+
+# 2.4 Linear Regression
+
+## Perform an initial analysis of the variable num_shares based on the others
+##by calculating the correlation coefficient between num_shares and each of the other variables ex-
+##cept status_type, status_published and num_reactions. Which one is the most correlated with
+##num_shares ?
+
+
+corr_matrix = df.corr(numeric_only=True)
+
+print('\nCorr matrix\n', corr_matrix)
+
+correlation = corr_matrix['num_shares']
+
+correlation = correlation.sort_values(ascending=False)
+
+print('\nVariable which is correlated the most with the ozone content num_shares\n\n', correlation)
+
+
+df_regsimple = smf.ols(formula='num_shares ~ num_loves', data=df).fit()
+coef = df_regsimple.params
+print('\nThe coefficients estimates are:\n', coef)
